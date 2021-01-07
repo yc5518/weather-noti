90471b48e7016856fc90456c7eba6957598d98c7
Chao Yang <chao.yang@coretex.com>
2021-01-07 13:28:57 +1300

    Missed commits

--- server/utils/textBeltSMSSender.js
+++ server/utils/textBeltSMSSender.js
@@ -1,11 +1,9 @@
 const axios = require('axios');
 
-const sendSMSViaTextBelt = async ({ destination, message, key }) => {
-  await axios.post('http://textbelt.com/text', {
-    phone: destination,
-    message,
-    key,
-  });
-};
+const sendSMSViaTextBelt = async ({ destination, message, key }) => axios.post('http://textbelt.com/text', {
+  phone: destination,
+  message,
+  key,
+});
 
 module.exports = sendSMSViaTextBelt;
